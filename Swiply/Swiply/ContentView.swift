b5801c8360b7c1aaad8553c8e96a799bb91457de
Chernykh Ivan <ivan.chernykh@kaspersky.com>
2024-02-16 00:13:28 +0300

    create project and networking package

---
+++ Swiply/Swiply/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiply
+//
+//  Created by Ivan Chernykh on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiply/Swiply/SwiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiplyApp.swift
+//  Swiply
+//
+//  Created by Ivan Chernykh on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
