eab1d29e51a9d3ce8a4d22243f3056205836e213
Chernykh Ivan <ivan.chernykh@kaspersky.com>
2024-02-17 16:04:10 +0300

    add composable architecture dependency

--- Swiply/Networking/Package.swift
+++ Swiply/Networking/Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "Networking",
+    platforms: [
+        .iOS(.v17),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Networking",
             targets: ["Networking"]),
     ],
+    dependencies: [.package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", branch: "observation-beta")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Networking"),
+            name: "Networking", dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]),
         .testTarget(
             name: "NetworkingTests",
             dependencies: ["Networking"]),
