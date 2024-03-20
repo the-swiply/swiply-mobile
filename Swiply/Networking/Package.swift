a98462a080ce8295b6b11a10050aabb5f1eb8468
Chernykh Ivan <ivan.chernykh@kaspersky.com>
2024-03-20 11:38:53 +0300

    add missing dependency to Networking package

--- Swiply/Networking/Package.swift
+++ Swiply/Networking/Package.swift
@@ -15,14 +15,17 @@ let package = Package(
             targets: ["Networking"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.9.2")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.9.2"),
+        .package(path: "../SYKeychain")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Networking", dependencies: [
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            name: "Networking",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                "SYKeychain"
             ]),
         .testTarget(
             name: "NetworkingTests",
