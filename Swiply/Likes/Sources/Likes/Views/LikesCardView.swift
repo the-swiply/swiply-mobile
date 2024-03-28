98dba1cd955faa627a8ff8097ffcd93ac473f9cb
Chernykh Ivan <ivan.chernykh@kaspersky.com>
2024-03-29 02:42:02 +0300

    fix card

--- Swiply/Likes/Sources/Likes/Views/LikesCardView.swift
+++ Swiply/Likes/Sources/Likes/Views/LikesCardView.swift
@@ -14,33 +14,41 @@ public struct LikesCardView: View {
             label: {
                 image
                     .centerCropped()
-                    .overlay {
-                        VStack(alignment: .leading) {
-                            Spacer()
-
-                            Text(name + "," + age)
-                                .foregroundStyle(.white)
-                                .font(.headline)
-                                .fontWeight(.semibold)
-                                .padding(.bottom, 8)
-
-                            HStack {
-                                Image(.dislike)
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-
-                                Spacer()
-
-                                Image(.like)
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                            }
-                        }
-                        .padding(.all, 12)
-                    }
                     .clipShape(RoundedRectangle(cornerRadius: 20))
             }
         )
+        .overlay {
+            VStack(alignment: .leading) {
+                Spacer()
+
+                Text(name + "," + age)
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 8)
+
+                HStack {
+                    Button {
+
+                    } label : {
+                        Image(.dislike)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                    }
+
+                    Spacer()
+
+                    Button {
+
+                    } label: {
+                        Image(.like)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                    }
+                }
+            }
+            .padding(.all, 12)
+        }
     }
 
 }
